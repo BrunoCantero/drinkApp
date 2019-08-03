dc7b54b2466fa0988e1eacf934ef80987aa1c17f
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2019-08-03 20:31:36 -0300

    Update drinks.js

--- src/reducers/drinks.js
+++ src/reducers/drinks.js
@@ -28,17 +28,3 @@ export function showDrinks ( state = initialState, action ){
             return state;
     }
 }
-
-/* export function showDrinks (state = initialState ,  action){
-    const { type, payload } = action;
-    switch (type){
-        case FETCHING_START:
-            return Object.assign({}, state, {isloading: true, error:false}); 
-        case FETCHING_SUCCESS:
-            return Object.assign({}, state, {isloading: false, list: payload, error:false}); 
-        case FETCHING_FAILED:
-            return Object.assign({}, state, {isloading: false, error:true}); 
-        default:
-            return state;
-    }
-} */
